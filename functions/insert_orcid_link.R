1b8b95f50aa4295be2ccd226708b57ec6d193939
martingerdin <martingerdin@gmail.com>
2024-04-30 14:15:57 +0200

    Start drafting statistical analysis plan

---
+++ functions/insert_orcid_link.R
@@ -0,0 +1,3 @@
+insert_orcid_link <- function(orcid.id) {
+    return(paste0("[![](https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png){height=16 width=16}](https://orcid.org/", orcid.id, ")"))
+}
