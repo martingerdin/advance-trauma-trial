f22676dde24edcb64aab98ac113f84ae1ac1ac81
martingerdin <martingerdin@gmail.com>
2024-10-20 23:36:07 +0530

    Update events in patient procedure table

--- functions/create_patient_procedure_table.R
+++ functions/create_patient_procedure_table.R
@@ -1,49 +1,57 @@
-create_patient_procedure_table <- function() {
-    ## Load packages
+create_patient_procedure_table <- function(save.to.disk = FALSE) {
+    # Load packages
     library(gt)
 
-    ## Define borrowed functions
+    # Define borrowed functions
     assert_that <- assertthat::assert_that
 
-    ## Define utility function to create follow up schedule
+    # Define utility function to create follow up schedule
     create_follow_up_schedule <- function(screening = FALSE,
-                                          follow.up.discharge = FALSE,
+                                          consenting = FALSE,
+                                          initial.assessment = FALSE,
+                                          in.hospital.care = FALSE,
                                           follow.up.7.days.discharge = FALSE,
                                           follow.up.30.days = FALSE,
                                           follow.up.90.days = FALSE,
                                           return.only.labels = FALSE) {
-        ## Check arguments
+        # Check arguments
         assert_that(is.logical(screening))
-        assert_that(is.logical(follow.up.discharge))
+        assert_that(is.logical(consenting))
+        assert_that(is.logical(initial.assessment))
+        assert_that(is.logical(in.hospital.care))
         assert_that(is.logical(follow.up.7.days.discharge))
         assert_that(is.logical(follow.up.30.days))
         assert_that(is.logical(follow.up.90.days))
         assert_that(is.logical(return.only.labels))
 
-        ## Create schedule
+        # Create schedule
         schedule <- c(
             screening = ifelse(screening, "√", ""),
-            follow.up.discharge = ifelse(follow.up.discharge, "√", ""),
+            consenting = ifelse(consenting, "√", ""),
+            initial.assessment = ifelse(initial.assessment, "√", ""),
+            in.hospital.care = ifelse(in.hospital.care, "√", ""),
             follow.up.7.days.discharge = ifelse(follow.up.7.days.discharge, "√", ""),
             follow.up.30.days = ifelse(follow.up.30.days, "√", ""),
             follow.up.90.days = ifelse(follow.up.90.days, "√", "")
         )
 
-        ## Define labels
+        # Define labels
         labels <- c(
             screening = "Screening",
-            follow.up.discharge = "Discharge",
+            consenting = "Consenting",
+            initial.assessment = "Initial assessment",
+            in.hospital.care = "In-hospital care",
             follow.up.7.days.discharge = "Within 7 days of discharge",
             follow.up.30.days = "30 days",
             follow.up.90.days = "90 days"
         )
 
-        ## Stop if the names of the schedule and labels do not match
+        # Stop if the names of the schedule and labels do not match
         if (!identical(names(schedule), names(labels))) {
             stop("The names of the schedule and labels do not match.")
         }
 
-        ## Return schedule or labels
+        # Return schedule or labels
         if (return.only.labels) {
             return(labels)
         } else {
@@ -51,41 +59,42 @@ create_patient_procedure_table <- function() {
         }
     }
 
-    ## Define follow up data
+    # Define follow up data
     procedure.list <- list(
         "Eligibility criteria" = create_follow_up_schedule(screening = TRUE),
-        "Study information" = create_follow_up_schedule(screening = TRUE),
-        "Informed consent" = create_follow_up_schedule(screening = TRUE),
-        "Baseline data collection" = create_follow_up_schedule(screening = TRUE),
+        "Study information" = create_follow_up_schedule(consenting = TRUE),
+        "Informed consent" = create_follow_up_schedule(consenting = TRUE),
+        "Baseline data collection" = create_follow_up_schedule(initial.assessment = TRUE),
+        "Prehospital data collection" = create_follow_up_schedule(initial.assessment = TRUE),
         "ATLS adherence" = create_follow_up_schedule(
-            screening = TRUE
+            initial.assessment = TRUE
         ),
         "ED data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            initial.assessment = TRUE
         ),
         "Hospital data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "Surgery data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "Imaging data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "Transfusion data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "Injury data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "Mortality data collection" = create_follow_up_schedule(
-            follow.up.discharge = TRUE,
+            in.hospital.care = TRUE,
             follow.up.7.days.discharge = TRUE,
             follow.up.30.days = TRUE,
             follow.up.90.days = TRUE
         ),
         "Assessment of safety events" = create_follow_up_schedule(
-            follow.up.discharge = TRUE
+            in.hospital.care = TRUE
         ),
         "EQ-5D/WHODAS" = create_follow_up_schedule(
             follow.up.7.days.discharge = TRUE,
@@ -107,7 +116,7 @@ create_patient_procedure_table <- function() {
     procedure.table <- gt(procedure.data) |>
         tab_spanner(
             label = "Follow up",
-            columns = c("Discharge", "Within 7 days of discharge", "30 days", "90 days")
+            columns = c("Within 7 days of discharge", "30 days", "90 days")
         ) |>
         tab_footnote(
             footnote = c(
@@ -147,5 +156,8 @@ create_patient_procedure_table <- function() {
         )
 
     ## Return follow up table and notes
+    if (save.to.disk) {
+        gtsave(procedure.table, "patient-procedure-table.html")
+    }
     return(procedure.table)
 }
